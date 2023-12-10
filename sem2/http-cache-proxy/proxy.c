d00ff20e016bfd6da5863a6061b2c1a5ac5254ac
Maxim <maxim.golubeff2014@gmail.com>
2023-12-10 14:37:58 +0700

    proxy

--- sem2/http-cache-proxy/proxy.c
+++ sem2/http-cache-proxy/proxy.c
@@ -4,7 +4,21 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <pthread.h>
 #include "proxy.h"
+#include "io.h"
+
+void proxy_readln(io_buffer_t *iob, void *usrbuf) {
+    ssize_t r = readln_b(iob, usrbuf, MAX_LINE_SIZE);
+    if (r < 0) {
+        if (errno == ECONNRESET) {
+            pthread_exit(NULL);
+        } else {
+            handle_error("failed to read data");
+        }
+    }
+}
+
 
 void parse_uri(char *uri, req_content *content) {
     char temp[MAX_LINE_SIZE];
@@ -27,3 +41,41 @@ void parse_uri(char *uri, req_content *content) {
     }
 
 }
+
+int read_req_hdr(io_buffer_t *iob, char *data) {
+    char buf[MAX_LINE_SIZE];
+    int ret = 0;
+
+    do {
+        proxy_readln(iob, buf);
+
+        if (!strcmp(buf, "\r\n"))
+            continue;
+
+        if (strstr(buf, "User-Agent:"))
+            continue;
+
+        if (strstr(buf, "Accept:"))
+            continue;
+
+        if (strstr(buf, "Accept-Encoding:"))
+            continue;
+
+        if (strstr(buf, "Connection:"))
+            continue;
+
+        if (strstr(buf, "Proxy-Connection:"))
+            continue;
+
+        if (strstr(buf, "Host:")) {
+            sprintf(data, "%s%s", data, buf);
+            ret = 1;
+            continue;
+        }
+
+        sprintf(data, "%s%s", data, buf);
+
+    } while (strcmp(buf, "\r\n"));
+    return ret;
+}
+
--- sem2/http-cache-proxy/proxy.h
+++ sem2/http-cache-proxy/proxy.h
@@ -6,11 +6,16 @@
 #define HTTP_CACHE_PROXY_PROXY_H
 
 #include "proxy_config.h"
+#include "io.h"
 
 typedef struct _req_content {
     char host[MAX_LINE_SIZE];
     char path[MAX_LINE_SIZE];
     int port;
 } req_content;
+
+void proxy_readln(io_buffer_t *iob, void *usrbuf);
+
+void parse_uri(char *uri, req_content *content);
 
 #endif //HTTP_CACHE_PROXY_PROXY_H
