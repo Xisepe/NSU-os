0058f6f3361a99b6d8511e818037d89944a0a0a5
Xisepe <maxim.golubeff2014@gmail.com>
2023-12-28 17:26:03 +0700

    proxy

---
+++ sem2/http-proxy-no-cache/main.c
@@ -0,0 +1,219 @@
+#include "proxy.h"
+
+void handle_err(const char *msg) {
+    fprintf(stderr, "App error: %s. Exiting thread: %d\n", msg, gettid());
+    pthread_exit(NULL);
+}
+
+
+void init_request(request_t *req) {
+    bzero(req->headers, sizeof(req->headers));
+    bzero(req->version, sizeof(req->version));
+    bzero(req->method, sizeof(req->method));
+    bzero(req->url.path, sizeof(req->url.path));
+    bzero(req->url.host, sizeof(req->url.host));
+}
+
+void parse_uri(char *uri, url_t *content) {
+    char temp[512];
+
+    if (strstr(uri, "http://") != NULL) {
+        sscanf(uri, "http://%[^/]%s", temp, content->path);
+    } else {
+        sscanf(uri, "%[^/]%s", temp, content->path);
+    }
+
+    if (strstr(temp, ":") != NULL) {
+        sscanf(temp, "%[^:]:%d", content->host, &content->port);
+    } else {
+        strcpy(content->host, temp);
+        content->port = 80;
+    }
+
+    if (!content->path[0]) {
+        strcpy(content->path, "./");
+    }
+
+}
+
+int open_connection(char *url) {
+    struct sockaddr_in target_addr;
+    memset(&target_addr, 0, sizeof(target_addr));
+    target_addr.sin_family = AF_INET;
+    target_addr.sin_port = htons(HTTP_PORT);
+
+    struct hostent *host = gethostbyname(url);
+    memcpy(&target_addr.sin_addr, host->h_addr_list[0], host->h_length);
+
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd == -1) {
+        handle_err("Cannot create socket for target connection");
+    }
+
+    if (connect(socket_fd, (struct sockaddr *) &target_addr, sizeof(target_addr)) == -1) {
+        char err[256];
+        sprintf(err, "Cannot establish connection with server: %s", url);
+        handle_err(err);
+    }
+    return socket_fd;
+}
+
+ssize_t readline_header(char *data, char *buf) {
+    char *cur = data;
+    char *end = strstr(cur, CRLF);
+    ssize_t n = end - cur;
+    if (n == 0) {
+        return 0;
+    }
+    strncpy(buf, cur, n);
+    buf[n] = '\0';
+    return n;
+}
+
+void parse_request_url(char *data, request_t *request) {
+    char tmp[MAX_URL_SIZE];
+    sscanf(data, "%s %s %s", request->method, tmp, request->version);
+    parse_uri(tmp, &request->url);
+}
+
+void parse_request(char *data, size_t data_size, request_t *req) {
+    char buf[MAX_BUFFER_SIZE];
+    bzero(buf, MAX_BUFFER_SIZE);
+    char *cur = data;
+
+    ssize_t read = readline_header(cur, buf);
+    cur += read + 2;
+    parse_request_url(buf, req);
+
+    int hostFlag = 0;
+
+    while (cur - data < data_size && (read = readline_header(cur, buf))) {
+        cur += read + 2;
+        if (strstr(buf, "Connection:")) {
+            continue;
+        }
+        if (strstr(buf, "Proxy-Connection:")) {
+            continue;
+        }
+        if (strstr(buf, "Host:")) {
+            hostFlag = 1;
+        }
+        strncat(req->headers, buf, read);
+        strcat(req->headers, "\r\n");
+    }
+    if (!hostFlag) {
+        char tmp[MAX_HEADER_SIZE];
+        bzero(tmp, sizeof(tmp));
+        sprintf(tmp, "Host: %s:%d\r\n", req->url.host, req->url.port);
+        strcat(req->headers, tmp);
+    }
+    strcat(req->headers, connection_hdr);
+    strcat(req->headers, proxy_conn_hdr);
+    strcat(req->headers, "\r\n");
+}
+
+void build_byte_request(request_t *req, char *buf) {
+    sprintf(buf, "%s %s %s\r\n%s", req->method, req->url.path, "HTTP/1.0", req->headers);
+}
+
+void *handle_request(void *arg) {
+    CHECK(pthread_detach(pthread_self()), "Cannot detach handler thread");
+
+    int socket_fd = (int) arg;
+    char buffer[MAX_BUFFER_SIZE];
+    bzero(buffer, sizeof(buffer));
+
+    ssize_t r = read(socket_fd, buffer, MAX_BUFFER_SIZE);
+    if (r == -1) {
+        handle_err("Cannot read request from client");
+    }
+    request_t request;
+    init_request(&request);
+    parse_request(buffer, r, &request);
+    bzero(buffer, sizeof(buffer));
+    build_byte_request(&request, buffer);
+    printf("%s", buffer);
+    int server_fd = open_connection(request.url.host);
+
+    ssize_t w = write(server_fd, buffer, strlen(buffer));
+    if (w == -1) {
+        char err[256];
+        sprintf(err, "Cannot send request to server: %s", request.url.host);
+        handle_err("Cannot send request to server");
+    }
+
+    bzero(buffer, sizeof(buffer));
+    while ((r = read(server_fd, buffer, sizeof(buffer))) > 0) {
+        write(socket_fd, buffer, r);
+    }
+
+    close(server_fd);
+    close(socket_fd);
+}
+
+int open_accept_listener(int port) {
+    int listen_fd, optval = 1;
+    struct sockaddr_in server_addr;
+
+    if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        return -1;
+    }
+
+    if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR,
+                   (const void *) &optval, sizeof(int)) < 0) {
+        return -1;
+    }
+
+    memset((char *) &server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons((unsigned short) port);
+    if (bind(listen_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+        return -1;
+    }
+
+    if (listen(listen_fd, 1024) < 0) {
+        return -1;
+    }
+    return listen_fd;
+}
+
+void sig_handler(int sig) {
+    printf("SIGPIPE signal trapped. Exiting thread.");
+    pthread_exit(NULL);
+}
+
+void setup_signal_handler() {
+    struct sigaction action, old_action;
+
+    action.sa_handler = sig_handler;
+    sigemptyset(&action.sa_mask); /* block sigs of type being handled */
+    action.sa_flags = SA_RESTART; /* restart syscalls if possible */
+
+    CHECK(sigaction(SIGPIPE, &action, &old_action), "failed to setup signal handler");
+}
+
+
+int main() {
+    setup_signal_handler();
+    int listen_fd = open_accept_listener(PORT);
+    if (listen_fd == -1) {
+        handle_err("Cannot create socket to accept connections");
+    }
+    while (1) {
+        struct sockaddr_in addr;
+        int len = sizeof(addr);
+        int conn = accept(listen_fd, (struct sockaddr *) &addr, (socklen_t *) &len);
+        if (conn == -1) {
+            fprintf(stderr, "Failed to accept connection\n");
+            continue;
+        }
+        pthread_t tid;
+        CHECK(
+                pthread_create(&tid, NULL, (void *) handle_request, (void *)conn ),
+                "cannot create thread for new connection"
+        );
+    }
+
+    return 0;
+}
---
+++ sem2/http-proxy-no-cache/proxy.h
@@ -0,0 +1,74 @@
+//
+// Created by Maxim on 28.12.2023.
+//
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
+#include <signal.h>
+
+#ifndef HTTP_PROXY_PROXY_H
+#define HTTP_PROXY_PROXY_H
+
+#define handle_error_en(en, msg) \
+               do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
+
+#define CHECK(x, msg) do { \
+  int retval = (x); \
+  if (retval != 0) { \
+    handle_error_en(retval, msg); \
+  } else { break; } \
+} while (0)
+
+#define HTTP_PORT 80
+#define PORT 5555
+
+#define MAX_BUFFER_SIZE 1 << 15
+#define MAX_METHOD_SIZE 32
+#define MAX_HOST_SIZE 256
+#define MAX_PATH_SIZE 1 << 14 //16kb
+#define MAX_URL_SIZE (MAX_HOST_SIZE + (MAX_PATH_SIZE))
+#define MAX_VERSION_SIZE 16
+#define MAX_HEADERS_SIZE 1 << 15 //32kb
+#define MAX_HEADER_SIZE 1 << 14 //16kb
+#define CRLF "\r\n"
+
+static const char *connection_hdr = "Connection: close\r\n";
+static const char *proxy_conn_hdr = "Proxy-Connection: close\r\n";
+
+typedef struct _url_t {
+    char host[MAX_HOST_SIZE];
+    char path[MAX_PATH_SIZE];
+    int port;
+} url_t;
+
+typedef struct _request_t {
+    char method[MAX_METHOD_SIZE];
+    char version[MAX_VERSION_SIZE];
+    url_t url;
+    char headers[MAX_HEADERS_SIZE];
+} request_t;
+
+void init_request(request_t *req);
+void parse_request(char *data, size_t data_size, request_t *request);
+void parse_request_url(char *data, request_t *request);
+void parse_uri(char *uri, url_t *content);
+void build_byte_request(request_t *req, char *buf);
+
+ssize_t readline_header(char *data, char *buf);
+
+int open_accept_listener(int port);
+int open_connection(char *url);
+void *handle_request(void *arg);
+
+void sig_handler(int sig);
+void setup_signal_handler();
+
+#endif //HTTP_PROXY_PROXY_H
